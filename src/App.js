b66824d4258237a404050307154fbed2a799e9e7
shmagi.onaini <onianishmagi@gmail.com>
2022-04-24 02:21:01 +0400

    init admin

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
-import React from "react";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-// React Router
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-// Routes
-import { PUBLIC_ROUTES } from "./utils/routes";
-import "antd/dist/antd.min.css";
-import "./App.scss";
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
-function App() {
+// ----------------------------------------------------------------------
+
+export default function App() {
   return (
-    <Router>
-      <div className={`app`}>
-        <div className="app-container">
-          <Switch>
-            {PUBLIC_ROUTES.map((route, index) => {
-              return (
-                <Route
-                  path={route.path + route.layout}
-                  component={route.component}
-                  key={route.path}
-                />
-              );
-            })}
-            <Route exact path="/">
-              <Redirect to="/home" />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <ScrollToTop />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeProvider>
   );
 }
-
-export default App;
-
-// <Redirect to="/NotFound" />
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from "./home/Home";
-export { default as ProductList } from "./productList/ProductList";
-export { default as Product } from "./product/Product";
